6ca3136aa20bf95c7f05fad7f4a521faa302a8cd
afodor88 <afodor88@gmail.com>
2015-11-14 21:32:55 +0200

    refactoring work file to work fisier

--- Learning/Challenge1.py
+++ Learning/Challenge1.py
@@ -11,12 +11,12 @@ reddituser = raw_input("And finally your reddit user?")
 print "Hi, your name is %s, your age is %i and your username is %s" % (name, age, reddituser)
 age = str(age)  # turning the age variable back to string so it can be concatenated with the new line when writing file
 
-file = open("userinfo.txt", "w")  # creating and opening file called userinfo.txt and assigning it to file variable
-file.write(name + "\n")  # writing in the file the containing of name variable
-file.write(age + "\n")  # writing in the file the containing of age variable
-file.write(reddituser + "\n")  # writing in the file the containing of reddituser variable
-file.close()  # closing file
+fisier = open("userinfo.txt", "w")  # creating and opening file called userinfo.txt and assigning it to file variable
+fisier.write(name + "\n")  # writing in the file the containing of name variable
+fisier.write(age + "\n")  # writing in the file the containing of age variable
+fisier.write(reddituser + "\n")  # writing in the file the containing of reddituser variable
+fisier.close()  # closing file
 
-file = open("userinfo.txt", "r")  # opening previously written file since you first need to open file before read
-print file.read()  # printing the containing of the previously written file
-file.close()
+fisier = open("userinfo.txt", "r")  # opening previously written file since you first need to open file before read
+print fisier.read()  # printing the containing of the previously written file
+fisier.close()
