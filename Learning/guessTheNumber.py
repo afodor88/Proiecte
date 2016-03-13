8973379013bbdd847e46e44453d60915aeab86b5
afodor88 <afodor88@gmail.com>
2016-03-13 19:16:45 +0200

    remove the debug line

--- Learning/guessTheNumber.py
+++ Learning/guessTheNumber.py
@@ -3,7 +3,7 @@ import random
 name = raw_input("May I know your name?")
 print "Hi " + name + ". We will play guess a number game. I'm thinking of a number between 1 and 20"
 number = random.randint(1, 20)
-print "DEBUG: " + "Secret number is: " + str(number)
+
 for numberOfGuesses in range(1, 7):
     guess = int(raw_input("Take a guess"))
     if guess > number:
