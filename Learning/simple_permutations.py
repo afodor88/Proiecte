1d4cb1f1d9a479a6ff27faeadd38140b031dd2cb
afodor88 <afodor88@gmail.com>
2016-03-20 21:56:32 +0200

    added

---
+++ Learning/simple_permutations.py
@@ -0,0 +1,4 @@
+for leftdigit in range(0,11):
+    for middledigit in range(0,11):
+        for rightdigit in range(0,11):
+            print str(leftdigit) + "-" + str(middledigit) + "-" + str(rightdigit)
