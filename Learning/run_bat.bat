86ebf55b55f91f008f9017e8a73f6ae23438ac55
afodor88 <afodor88@gmail.com>
2016-04-09 23:22:22 +0300

    added a comment about the program

--- Learning/run_bat.bat
+++ Learning/run_bat.bat
@@ -1,2 +1,11 @@
-@python D:\eclipse\workspace\Proiecte\trunk\Learning\Challenge1_function.py %*
+REM This is an example of how to run a python command from a batch file
+
+@python D:\eclipse\workspace\Proiecte\trunk\Learning\Challenge1_function.py %* 
 @pause
+
+GOTO:EndComment
+This is a comment.
+We put @ in front of the python command.
+%* means we are forwarding the arguments from the command - if there are any.
+We use pause to keep the command line open after running a command.
+:EndComment
