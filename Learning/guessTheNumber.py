08a6cced790a644c7c741b9bfbd9e2e2a7b07dcb
afodor88 <afodor88@gmail.com>
2016-04-21 00:38:33 +0300

    adding more logic and naming in the code

--- Learning/guessTheNumber.py
+++ Learning/guessTheNumber.py
@@ -2,21 +2,21 @@ import random
 
 name = raw_input("May I know your name?")
 print "Hi " + name + ". We will play guess a number game. I'm thinking of a number between 1 and 20"
-number = random.randint(1, 20)
+secretnumber = random.randint(1, 20)
 
 # Ask the user to guess 6 times
 for numberOfGuesses in range(1, 7):
     try:
         guess = int(raw_input("Take a guess"))
-        if guess > number:
+        if secretnumber < guess:
             print "The number I'm thinking is lower than " + str(guess) + "."
-        elif guess < number:
+        elif secretnumber > guess:
             print "The number I'm thinking is higher than " + str(guess) + "."
         else:
             break  # Number is correct or number of guess max has reached
     except ValueError:
         print "This field accepts only numbers"
-if guess == number:
+if guess == secretnumber:
     print "You guessed the number! You guessed it in " + str(numberOfGuesses) + " tries!"
 else:
-    print "You have failed. The correct number was " + str(number) + "."
+    print "You have failed. The correct number was " + str(secretnumber) + "."
