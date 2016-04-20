b748310453866afe0ec4101fbcf21e973d0ccf1c
afodor88 <afodor88@gmail.com>
2016-04-20 14:31:40 +0300

    transformed into a function

--- Learning/Challenge2.py
+++ Learning/Challenge2.py
@@ -4,11 +4,36 @@ so your challenge for today is to create a calculator application that has use i
 It might be an interest calculator, or it might be something that you can use in the classroom.
 For example, if you were in physics class, you might want to make a F = M * A calc. 
 EXTRA CREDIT: make the calculator have multiple functions! Not only should it be able to calculate F = M * A, but also A = F/M, and M = F/A!"""
-   
-mass = raw_input('This calculator measures the force. Please input the mass of the object')  
-acceleration = raw_input('Now please input the acceleration')
-mass = int(mass)
-acceleration = int(acceleration)
-force = mass * acceleration
 
-print 'The Force calculated for a object of mass ' + str(mass) + ' and acceleration ' + str(acceleration) + ' is ' + str(force) + '.'    
+
+
+userchoice = raw_input('What do you want to calculate?\nmass, acceleration or force')
+
+def calculator():
+    if userchoice == 'mass':
+        acceleration = int(raw_input('What will be the acceleration?'))
+        force = int(raw_input('And what it will be the force?'))
+        return float(force) / float(acceleration)
+
+    elif userchoice == 'force':
+        acceleration = int(raw_input('What will be the acceleration?'))
+        mass = int(raw_input('And what it will be the mass?'))
+        return float(mass) * float(acceleration)
+
+    elif userchoice == 'acceleration':
+        force = int(raw_input('And what it will be the force?'))
+        mass = int(raw_input('And what it will be the mass?'))
+        return float(force)/float(mass)
+
+    else:
+        return "You didn't choose any of the available options"
+
+
+print calculator()
+
+        
+        
+
+
+    
+
